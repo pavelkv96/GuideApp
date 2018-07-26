91ee6784fcdf312cd0339806d620ad1f8b802633
Kalinin_PV <pavelkv96@gmail.com>
2018-07-26 09:01:45 +0300

    Initial project

---
+++ app/src/main/java/com/grsu/guideapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.grsu.guideapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
