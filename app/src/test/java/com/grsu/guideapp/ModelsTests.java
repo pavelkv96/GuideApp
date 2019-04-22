a5632b8201ed38ad69daebb2e8627f5efa5f0234
Kalinin_PV <pavelkv96@gmail.com>
2019-04-22 16:18:41 +0300

    Updated: 1)Versions libraries 2)Change min support version api to 21 (Android 5.0) 3)Version database 4)Permission and screen orientation in manifest file 5)Settings xml
    Added:
    1)Api key
    2)Implementation for image library
    3)Image for local route
    4)Icons for download, for exit in splash
    5)Item "About" in menu
    6)Some strings in string resources for default and russian language
    
    Removed:
    1)Unnecessary tests
    2)Styles for 21 api
    
    Continuing in the next commit

--- app/src/test/java/com/grsu/guideapp/ModelsTests.java
+++ app/src/test/java/com/grsu/guideapp/ModelsTests.java
@@ -1,19 +1,10 @@
 package com.grsu.guideapp;
 
-import com.google.android.gms.maps.model.LatLng;
-import com.grsu.guideapp.models.Polyline;
 import com.grsu.guideapp.network.APIService;
-import com.grsu.guideapp.network.model.Category;
 import com.grsu.guideapp.network.model.Datum;
-import com.grsu.guideapp.network.model.Turn;
-import com.grsu.guideapp.network.model.Value;
-import com.grsu.guideapp.utils.CryptoUtils;
 import com.grsu.guideapp.utils.Mocks;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -37,103 +28,40 @@ public class ModelsTests {
     }
 
     @Test
-    public void testTeacherScheduleModel() throws IOException {
-        Category category = getData();
-        if (category != null) {
-            Value value = category.getData().get(1).getData().getRoute().getValue();
-            CryptoUtils.encodeP(value.getLimLeft().getLatLng());
-            CryptoUtils.encodeP(value.getLimRight().getLatLng());
+    public void test3() throws IOException {
+        Call<Datum> call = Mocks.getCall("test3.json", Datum.class);
+        Mockito.when(mockScheduleApi.getPoi(1, "")).thenReturn(call);
+        Call<Datum> scheduleCall = mockScheduleApi.getPoi(1, "");
+        Datum datum = scheduleCall.execute().body();
+        if (datum != null) {
+            /*Value value = datum.getData().getRoute().getValue();
 
-            /*List<Turn> points = value.getPoints();
-            int id = 0;
-            for (Turn point : points) {
-                System.out.println(String.format("%3s  %s", id, point));
-                id++;
+            int id = 243;
+            List<Objects> objectsList = value.getObjects();
+
+            int index = objectsList.indexOf(new Objects(id));
+            System.out.println(objectsList.get(index));*/
+
+
+            /*List<Turn1> points = value.getPoints();
+
+            Set<Point> set = new ArraySet<>();
+
+            for (Turn1 turn1 : points) {
+                Point start = turn1.getStart();
+                if (start.getObjects() != null) {
+                    set.add(start);
+                }
+            }
+
+            Point end = points.get(points.size() - 1).getEnd();
+            if (end.getObjects() != null) {
+                set.add(end);
+            }
+
+            for (LatLong latLong : set) {
+                System.out.println(latLong.toString());
             }*/
         }
     }
-
-    @Test
-    public void converterData() throws IOException {
-        Category category = getData();
-        if (category != null) {
-            Datum datum = category.getData().get(1);
-//            datum.getId();//id route
-//
-//            datum.getData().getName().getFull();//full name route
-//            datum.getData().getName().getShort();//short name route
-//            datum.getData().getAbout().getFull();//full description route
-//            datum.getData().getAbout().getShort();//short description route
-//
-//            datum.getData().getRoute().getValue().getLimLeft();//left bounds
-//            datum.getData().getRoute().getValue().getLimRight();//right bounds
-//
-//            datum.getData().getRoute().getValue().getPoints();//list points
-//            datum.getData().getRoute().getValue().getObjects();//list poi
-
-            List<Turn> points = datum.getData().getRoute().getValue().getPoints();
-            /*int id = 0;
-            for (Turn point : points) {
-                System.out.println(String.format("%3s  %s", id, point));
-                id++;
-            }
-            System.out.println("-------------------");*/
-
-            for (Polyline polyline : getRoute(points)) {
-                System.out.println(polyline.toString());
-            }
-        }
-    }
-
-    @Test
-    public void test() {
-        String test1 = "SELECT id_line FROM lines WHERE polyline = 'wotfI_jopCQDQDQDOBQDQDQDQBODQDQDQBQDQDQDOBQDQDQDQDQDODQDQDQFQDODQDQDQDQDODQDQDQDQDQDMD'";
-
-        String query = "SELECT id_line FROM lines WHERE polyline = \'%s\'";
-
-        String test = String.format(query,
-                "wotfI_jopCQDQDQDOBQDQDQDQBODQDQDQBQDQDQDOBQDQDQDQDQDODQDQDQFQDODQDQDQDQDODQDQDQDQDQDMD");
-
-        Assert.assertEquals(test,test1);
-    }
-
-    private List<Polyline> getRoute(List<Turn> turns) {
-        List<Integer> idTurns = new ArrayList<>();
-        idTurns.add(0);
-        for (int i = 1; i < turns.size() - 1; i++) {
-            if (turns.get(i).getObjects() != null) {
-                idTurns.add(i);
-            }
-        }
-        idTurns.add(turns.size() - 1);
-
-        List<Polyline> polylines = new ArrayList<>();
-
-        for (int i = 1; i < idTurns.size(); i++) {
-            Polyline polyline = new Polyline();
-            Integer start = idTurns.get(i - 1);
-            Integer end = idTurns.get(i);
-            polyline.setStart(CryptoUtils.encodeP(turns.get(start).getLatLng()));
-            polyline.setEnd(CryptoUtils.encodeP(turns.get(end).getLatLng()));
-            List<Turn> subList = turns.subList(start, end + 1);
-            List<LatLng> latLngs = new ArrayList<>();
-            for (int j = 0; j < subList.size(); j++) {
-                latLngs.add(subList.get(j).getLatLng());
-            }
-
-            polyline.setPolyline(CryptoUtils.encodeL(latLngs));
-            latLngs.clear();
-            polylines.add(polyline);
-        }
-
-        return polylines;
-    }
-
-    private Category getData() throws IOException {
-        Call<Category> call = Mocks.getCall("test.json", Category.class);
-        Mockito.when(mockScheduleApi.getUsers("")).thenReturn(call);
-        Call<Category> scheduleCall = mockScheduleApi.getUsers("");
-        return scheduleCall.execute().body();
-    }
-
 }
